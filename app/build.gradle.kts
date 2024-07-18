b18ebc343d08a7dcca61bb992debf3bf07ae2f80
Divarizky <divarizky28@gmail.com>
2024-07-18 19:18:27 +0700

    Add new Dependencies: - Jetpack Compose - TensorFlow Lite - ViewModel
    Add new permission in AndroidManifest.xml:
    - READ EXTERNAL STORAGE
    - WRITE EXTERNAL STORAGE
    
    create a AutismDetectionScreen.kt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        mlModelBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -59,6 +60,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.tensorflow.lite.support)
+    implementation(libs.tensorflow.lite.metadata)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -67,4 +71,17 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(kotlin("script-runtime"))
+
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.4.2")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+
+    // TensorFlow
+    implementation("org.tensorflow:tensorflow-lite:2.9.0")
+
+    // ViewModel for Compose
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.8")
 }
--- app/src/main/java/com/application/divarizky/autismdetection/MainActivity.kt
+++ app/src/main/java/com/application/divarizky/autismdetection/MainActivity.kt
@@ -3,45 +3,18 @@ package com.application.divarizky.autismdetection
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.application.divarizky.autismdetection.ui.screens.AutismDetectionScreen
 import com.application.divarizky.autismdetection.ui.theme.AutismDetectionTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             AutismDetectionTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AutismDetectionScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutismDetectionTheme {
-        Greeting("Android")
     }
 }
